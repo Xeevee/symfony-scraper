8b36e6a9bb4b9896f3125cc9c5227465f19f63b4
Jack Neary <xeeeveee@gmail.com>
2016-08-29 20:53:38 +0100

    add: Product & ProductCollection Entities

---
+++ src/AppBundle/Entities/Product.php
@@ -0,0 +1,164 @@
+<?php
+
+namespace AppBundle\Entities;
+
+class Product {
+
+	/**
+	 * The product title
+	 *
+	 * @var string
+	 */
+	private $title;
+
+	/**
+	 * The products page size
+	 *
+	 * @var mixed
+	 */
+	private $size;
+
+	/**
+	 * The products price per unit
+	 *
+	 * @var mixed
+	 */
+	private $unitPrice;
+
+	/**
+	 * The products description
+	 *
+	 * @var string
+	 */
+	private $description;
+
+	/**
+	 * Product constructor.
+	 *
+	 * Will attempt to populate the properties from an associative array on construction
+	 *
+	 * @param array $data
+	 */
+	public function __construct( array $data = [ ] ) {
+		if ( isset( $data['title'] ) ) {
+			$this->setTitle( $data['title'] );
+		}
+
+		if ( isset( $data['size'] ) ) {
+			$this->setSize( $data['size'] );
+		}
+
+		if ( isset( $data['unitPrice'] ) ) {
+			$this->setUnitPrice( $data['unitPrice'] );
+		}
+
+		if ( isset( $data['description'] ) ) {
+			$this->setDescription( $data['description'] );
+		}
+	}
+
+	/**
+	 * Get the product's title
+	 *
+	 * @return string
+	 */
+	public function getTitle() {
+		return $this->title;
+	}
+
+	/**
+	 * Set the title
+	 *
+	 * @param string $title
+	 *
+	 * @return $this
+	 */
+	public function setTitle( $title ) {
+		if ( is_string( $title ) ) {
+			$this->title = $title;
+		}
+
+		return $this;
+	}
+
+	/**
+	 * Get the product's title
+	 *
+	 * Will always be numeric
+	 *
+	 * @return mixed
+	 */
+	public function getSize() {
+		return $this->size;
+	}
+
+	/**
+	 * Set the size
+	 *
+	 * Must be numeric
+	 *
+	 * @param mixed $size
+	 *
+	 * @return $this
+	 */
+	public function setSize( $size ) {
+		if ( is_numeric( $size ) ) {
+			$this->size = $size;
+		}
+
+		return $this;
+	}
+
+	/**
+	 * Get the product's unit price
+	 *
+	 * Will always be numeric
+	 *
+	 * @return mixed
+	 */
+	public function getUnitPrice() {
+		return $this->unitPrice;
+	}
+
+	/**
+	 * Set the unit price
+	 *
+	 * Must be numeric
+	 *
+	 * @param mixed $unitPrice
+	 *
+	 * @return $this
+	 */
+	public function setUnitPrice( $unitPrice ) {
+		if ( is_numeric( $unitPrice ) ) {
+			$this->unitPrice = $unitPrice;
+		}
+
+		return $this;
+	}
+
+	/**
+	 * Get the product's description
+	 *
+	 * @return string
+	 */
+	public function getDescription() {
+		return $this->description;
+	}
+
+	/**
+	 * Set the description
+	 *
+	 * @param string $description
+	 *
+	 * @return $this
+	 */
+	public function setDescription( $description ) {
+		if ( is_string( $description ) ) {
+			$this->description = $description;
+		}
+
+		return $this;
+	}
+
+}
---
+++ src/AppBundle/Entities/ProductCollection.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace AppBundle\Entities;
+
+class ProductCollection {
+
+	/**
+	 * The products
+	 *
+	 * @var array
+	 */
+	private $products = [ ];
+
+	/**
+	 * The total unit price of the products
+	 *
+	 * @var float
+	 */
+	private $total;
+
+	/**
+	 * ProductCollection constructor.
+	 *
+	 * Will attempt to add products from an array on construction
+	 *
+	 * @param array $products
+	 */
+	public function __construct( array $products = [ ] ) {
+		$this->addMultiple( $products );
+	}
+
+	/**
+	 * Get the products
+	 *
+	 * @return array
+	 */
+	public function getProducts() {
+		return $this->products;
+	}
+
+	/**
+	 * Adds a single product
+	 *
+	 * Updates the total property
+	 *
+	 * @param Product $product
+	 *
+	 * @return $this
+	 */
+	public function add( Product $product ) {
+
+		$this->products[] = $product;
+		$this->total += $product->getUnitPrice();
+
+		return $this;
+	}
+
+	/**
+	 * Adds products from an array
+	 *
+	 * @param array $products
+	 *
+	 * @return $this
+	 */
+	public function addMultiple( array $products ) {
+		foreach ( $products as $product ) {
+			if ( $product instanceof Product ) {
+				$this->add( $product );
+			}
+		}
+
+		return $this;
+	}
+
+	/**
+	 * Removes a product by key
+	 *
+	 * Updates the total property
+	 *
+	 * @param $key
+	 *
+	 * @return $this
+	 */
+	public function remove( $key ) {
+		if ( isset( $this->products[ $key ] ) ) {
+			$this->total -= $this->products[ $key ]['unitPrice'];
+			unset( $this->products[ $key ] );
+		}
+
+		return $this;
+	}
+
+	/**
+	 * Clears the products
+	 *
+	 * Also resets the total to 0
+	 *
+	 * @return $this
+	 */
+	public function clear() {
+		$this->total    = 0;
+		$this->products = [ ];
+
+		return $this;
+	}
+
+	/**
+	 * Get the total
+	 * 
+	 * @return mixed
+	 */
+	public function getTotal() {
+		return $this->total;
+	}
+}
