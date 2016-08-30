467e46d2c3e117cff969f05d28c5ffaeaa14eba0
Jack Neary <xeeeveee@gmail.com>
2016-08-30 21:07:28 +0100

    update: code style

--- src/AppBundle/Tests/Services/Scraper/Product/ScraperTest.php
+++ src/AppBundle/Tests/Services/Scraper/Product/ScraperTest.php
@@ -9,47 +9,47 @@ class ScraperTest extends \PHPUnit_Framework_TestCase {
 	private $sampleDocument = '<div class="productTitleDescriptionContainer"><h1>Some title</h1><p class="pricePerUnit">£3.50<abbr title="per">/</abbr><abbr title="unit"><span class="pricePerUnitUnit">unit</span></abbr></p><div id="information"><htmlcontent><div class="productText">A description</div></htmlcontent></div></div>';
 
 	public function testGetTitle() {
-		$scraper = new Scraper($this->sampleDocument);
-		$this->assertEquals('Some title', $scraper->getTitle());
+		$scraper = new Scraper( $this->sampleDocument );
+		$this->assertEquals( 'Some title', $scraper->getTitle() );
 	}
 
 	/**
 	 * @expectedException InvalidArgumentException
 	 */
 	public function testGetTitleNotFound() {
-		$scraper = new Scraper('');
+		$scraper = new Scraper( '' );
 		$scraper->getTitle();
 	}
 
 	public function testGetUnitPrice() {
-		$scraper = new Scraper($this->sampleDocument);
-		$this->assertEquals('3.50', $scraper->getUnitPrice());
+		$scraper = new Scraper( $this->sampleDocument );
+		$this->assertEquals( '3.50', $scraper->getUnitPrice() );
 	}
 
 	/**
 	 * @expectedException InvalidArgumentException
 	 */
 	public function testGetUnitPriceNotFound() {
-		$scraper = new Scraper('');
+		$scraper = new Scraper( '' );
 		$scraper->getUnitPrice();
 	}
 
 	public function testGetUnitPriceRegExMatched() {
 
-		$scraper = new Scraper('<p class="pricePerUnit">Wrong format</p>');
-		$this->assertEquals('', $scraper->getUnitPrice());
+		$scraper = new Scraper( '<p class="pricePerUnit">Wrong format</p>' );
+		$this->assertEquals( '', $scraper->getUnitPrice() );
 	}
 
 	public function testGetDescription() {
-		$scraper = new Scraper($this->sampleDocument);
-		$this->assertEquals('A description', $scraper->getDescription());
+		$scraper = new Scraper( $this->sampleDocument );
+		$this->assertEquals( 'A description', $scraper->getDescription() );
 	}
 
 	/**
 	 * @expectedException InvalidArgumentException
 	 */
 	public function testGetDescriptionNotFound() {
-		$scraper = new Scraper('');
+		$scraper = new Scraper( '' );
 		$scraper->getDescription();
 	}
 
